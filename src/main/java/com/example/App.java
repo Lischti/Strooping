3cbc1190d31dfe76b687fd354c25ba4b78b2bfe2
Luca List <lu121lis@htwg-konstanz.de>
2023-12-20 18:04:07 +0100

    Gui startet hier

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,10 +2,12 @@ package com.example;
 
 import io.javalin.Javalin;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+        new Gui();
+        
+        
+    /*     
         // Erstelle eine Javalin-App
         Javalin app = Javalin.create();
 
@@ -13,6 +15,6 @@ public class App
         
 
         // Starte den Server auf Port 7000
-        app.start(7000);
+        app.start(7000); */
     }
 }
